0393a6e78d75ebfe483f43f7cbdf4e01a9919e71
Abril Palencia <pal18198@uvg.edu.gt>
2020-05-20 22:52:18 -0600

    Proyectos y redes

--- pagina_personal/src/Principal.jsx
+++ pagina_personal/src/Principal.jsx
@@ -3,8 +3,13 @@ import './style.css';
 
 const Pagina = () => {
 
-  const cartas = [1, 2, 3, 4]
-  const Carta = ({texto}) => {
+  const cartas = [
+    {id: 1, imagen: "Proyecto de Base de Datos", des: ""},
+    {id: 2, imagen: "Juego de Memoria", des: "Un siemple juego de memoria con 16 cartas hecho con React."},
+    {id: 3, imagen: "Juego de Laberinto", des: "Un laberinto Random jalado del servidor hecho con React."},
+    {id: 4, imagen: "Imagen puro CSS", des: "Una imagen de un panda hecho solamente con CSS."},
+  ]
+  const Carta = ({texto, titulo, descrip}) => {
     
     return (
     <div className="style_carta" >
@@ -12,7 +17,8 @@ const Pagina = () => {
       <div className="thefront"><h1>{texto}</h1>
       </div>
         <div className="theback">
-          <p>aqui va el proyecto.</p>
+          <h4>{titulo}</h4>
+          <p>{descrip}</p>
         </div>
       </div>
     )
@@ -33,14 +39,14 @@ const Pagina = () => {
           Holi, mi nombre es Abril Palencia. Tengo 21 años de edad y estudio<br></br> <b>Ingeniería en Ciencia de la Computación y TI </b>
           en la Universidad del Valle de Guatemala. <br></br>Me gusta mucho bailar. <br></br>Soy una persona muy sociable y alegre.</p>
         <div className="redes">
-          <a href="https://www.instagram.com/_abrilpal/" className="link"><img src="/src/instagram.png"></img><span className="sp">Instagram</span></a>
-          <a href="https://github.com/AbrilPal" className="link"><img src="/src/github.png"></img><span className="sp">Github</span></a>
-          <a href="https://www.linkedin.com/in/abril-palencia-517a411a7/" className="link"><img src="/src/Linkedin.png"></img><span className="sp">Linkedin</span></a>
+          <a href="https://www.instagram.com/_abrilpal/" className="link"><img src="insta.png" className="logo"></img><span className="sp">Instagram</span></a>
+          <a href="https://github.com/AbrilPal" className="link"><img src="github.png" className="logo"></img><span className="sp">Github</span></a>
+          <a href="https://www.linkedin.com/in/abril-palencia-517a411a7/" className="link"><img src="Linkedin.png" className="logo2"></img><span className="sp">Linkedin</span></a>
         </div>
       </div>
       <div className="titulo"><br></br><br></br>Proyectos...</div>
       <div className="contenedor">
-          {cartas.map(() =><Carta texto = "</3"/>)}
+          {cartas.map((cartass) =><Carta texto = "</3" key={cartass.id} titulo={cartass.imagen} descrip={cartass.des} />)}
       </div>
     </div>
   )
